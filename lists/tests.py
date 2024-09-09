75abe9e617098ae59c0b81fd02b1e059b1f05361
gadi9889 <gadi8998@gmail.com>
2024-09-09 22:25:14 +0300

    removed redundent tests, added new /lists/new page for list creation

--- lists/tests.py
+++ lists/tests.py
@@ -6,26 +6,30 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
-        response = self.client.get("/")
-        self.assertContains(response, "item 1")
-        self.assertContains(response, "item 2")
-    
+class NewListTest(TestCase):
     def test_can_save_POST_req(self):
-        self.client.post("/", data={"item_text": "A new list item"})
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
     
     def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text":"A new list item"})
-        self.assertRedirects(response, "/")
+        response = self.client.post("/lists/new", data={"item_text":"A new list item"})
+        self.assertRedirects(response, "/lists/only-list/")
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/only-list/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="item 1")
+        Item.objects.create(text="item 2")
+
+        response = self.client.get("/lists/only-list/")
+
+        self.assertContains(response, "item 1")
+        self.assertContains(response, "item 2")
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -45,4 +49,3 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text, "the first (ever) list Item")
         self.assertEqual(second_saved_item.text, "the second list Item")
-    
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from lists.views import home_page
+from lists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name="home"),
+    path('', views.home_page, name="home"),
+    path('lists/new', views.new_list, name="new_list"),
+    path('lists/only-list/', views.view_list, name="view_list"),
 ]
